cf5311001286a40155928dff4fac5009ef6cebbd
kevinlcaiug <kevin.l.cai.ug@dartmouth.edu>
2021-10-20 11:56:17 -0400

    Removed MetaCoin contracts, deployment and test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,8 @@
 const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
 const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.link(ConvertLib, HelloWorld);
   deployer.deploy(HelloWorld);
 };
