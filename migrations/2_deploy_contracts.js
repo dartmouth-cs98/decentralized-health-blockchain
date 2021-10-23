2fd8f1bae8a05fb91d7fb2d535db11d4843c986f
kevinlcaiug <kevin.l.cai.ug@dartmouth.edu>
2021-10-23 17:03:51 -0400

    Deployment of Patient and Doctor contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const HelloWorld = artifacts.require("HelloWorld");
+const Patient = artifacts.require("Patient");
+const Doctor = artifacts.require("Doctor");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, HelloWorld);
   deployer.deploy(HelloWorld);
+  deployer.deploy(Patient);
+  deployer.deploy(Doctor);
 };
