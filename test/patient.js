ddf716d40ce694787329ce9abdd6dbb64c9c6a95
Loaner User <dgrkotsonis@gmail.com>
2021-10-25 12:09:43 -0400

    resolving conflicts to assist with merging to main

---
+++ test/patient.js
@@ -0,0 +1,15 @@
+const Patient = artifacts.require("Patient");
+
+contract('Patient', (accounts) => {
+    // Test Patient.sol contract
+    it('creates a new patient by calling the signupPatient method', async () => {
+        // Wait for the contract to be deployed
+        const patientInstance = await Patient.deployed();
+        // Call signupGreeting function
+        const patient1 = await patientInstance.signupPatient('Dionysios Kotsonis', 12);
+        // 
+        console.log("======================");
+        console.log(typeof(accounts[0]));
+        assert.equal(patient1.receipt.from.toUpperCase(), accounts[0].toUpperCase());
+    }); 
+});
