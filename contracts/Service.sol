f337015f74df19866da45d331fc489c3d473965b
Loaner User <dgrkotsonis@gmail.com>
2021-10-24 21:49:16 -0400

    adding method to allow doctor to get info about a specific patient

--- contracts/Service.sol
+++ contracts/Service.sol
@@ -11,11 +11,28 @@ contract Service is Doctor, Patient, File {
     // message sender
     address private owner;
     
+    // set owner
     constructor() public{
         owner = msg.sender;
+    }
+
+    // make sure that the sender is the owner
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
     }
     
     // methods to implement: 
     // give access to a doctor, add a file, get patient/doctor info from a file?
     // want to have checks as well
+
+    // allow doctor to get info about a specific patient
+    function getPatientInfoForDoctor(address patient) public view checkPatient(patient) checkDoctor(msg.sender) returns(string memory , uint8, address, bytes32[] memory ){
+        patient memory p = patients[patient];
+
+        require(patientToDoctor[patient][msg.sender] > 0);
+
+        return (p.name, p.age, p.addr, p.files);
+    }
+
 }
