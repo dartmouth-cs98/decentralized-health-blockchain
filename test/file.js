40fc6b5feb914ef09b2925193e3634eda7931e75
kevinlcaiug <kevin.l.cai.ug@dartmouth.edu>
2021-10-24 21:54:39 -0400

    Created file test file

---
+++ test/file.js
@@ -0,0 +1,13 @@
+const File = artifacts.require("File");
+
+contract('File', (accounts) => {
+//   // Test HelloWorld.sol contract
+//   it('greeting variable should be initialized to "Hello World!"', async () => {
+//     // Wait for the contract to be deployed
+//     const helloWorldInstance = await HelloWorld.deployed();
+//     // Call getGreeting function
+//     const greeting = await helloWorldInstance.getGreeting();
+//     // Check the value is initialized.
+//     assert.equal(greeting, "Hello World!", "String incorrectly initialized.");
+//   });
+});
