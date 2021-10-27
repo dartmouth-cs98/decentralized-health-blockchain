1e04407532d0c7595014ec9cf387ca007a86c316
kevinlcaiug <kevin.l.cai.ug@dartmouth.edu>
2021-10-27 17:04:14 -0400

    Added to patient testing for getPatientInfo function

--- test/patient.js
+++ test/patient.js
@@ -11,5 +11,11 @@ contract('Patient', (accounts) => {
         console.log("======================");
         console.log(typeof(accounts[0]));
         assert.equal(patient1.receipt.from.toUpperCase(), accounts[0].toUpperCase());
+
+        const p = await patientInstance.getPatientInfo();
+        console.log(p);
+        assert.equal(p[0], 'Dionysios Kotsonis');
+        console.log(typeof(p[1]));
+        assert.equal(p[2], 0);
     }); 
 });
