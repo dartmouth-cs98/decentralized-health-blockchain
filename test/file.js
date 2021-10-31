e3db02fc14b21478c91ecea1c3b5d1a0e9e9ae4a
dgrkotsonis <63176396+dgrkotsonis@users.noreply.github.com>
2021-10-31 16:34:42 -0400

    wrote tests for file contract. Verified proper storage of file data

--- test/file.js
+++ test/file.js
@@ -6,10 +6,21 @@ contract('File', (accounts) => {
     // Wait for the contract to be deployed
     const fileInstance = await File.deployed();
     // Call testFile function
-    const tfile = await fileInstance.testFile();
+    const tfile = await fileInstance.testFile("HIV: Negative|Hepatitus C: Negative|Coronavirus: Positive","blood test 1");
     // 
     console.log("======================");
-    console.log(tfile);
-    assert.equal(tfile[0], 'blood test 1');
+    // console.log(tfile);
+    console.log(tfile)
+
+    const hash = await fileInstance.getFileHash("HIV: Negative|Hepatitus C: Negative|Coronavirus: Positive");
+    const file_name = await fileInstance.getFileName(hash);
+    
+    assert.equal(file_name,"blood test 1");
+
+    // var reader = new FileReader();
+    // reader.readAsText();
+    // const fetch = require('node-fetch');
+    // fetch('./testfiles/filetest.txt').then(response => response.text()).then(text => console.log(text))
+  // outputs the content of the text file
   });
 });
