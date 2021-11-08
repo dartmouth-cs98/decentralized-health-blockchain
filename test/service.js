236b1acfd3bda91f05e834621d4ed8a894f02135
kevinlcaiug <kevin.l.cai.ug@dartmouth.edu>
2021-11-08 01:35:31 -0500

    migrated testing

--- test/service.js
+++ test/service.js
@@ -14,17 +14,59 @@ contract('Service', (accounts) => {
     assert.equal(tservice.toUpperCase(), accounts[0].toUpperCase());
   });
 
-  it('tests addFile method creates a new file in patients files array', async () => {
-    const serviceInstance = await Service.deployed();
-    console.log("1");
-    // create patient instance
-    const p = await serviceInstance.signupPatient("Dionysios Kotsonis", 12);
-    console.log("2");
-    // create doctor instance
-    const d = await serviceInstance.signupDoctor("Kevin Cai", 21);
-    console.log("3");
-    // add file to patient's file list
-    const f = await serviceInstance.addFile("Blood Test 1", "Blood Test", );
-    console.log("4");
+  // Doctor tests
+  it('creates a new doctor by calling the signupDoctor method', async () => {
+    // Wait for the contract to be deployed
+    const doctorInstance = await Service.deployed();
+    // Call signupGreeting function
+    const doctor1 = await doctorInstance.signupDoctor('Dionysios Kotsonis', "MGH Boston");
+    // 
+    console.log("======================");
+    console.log(doctor1[0]);
+    assert.equal(doctor1.receipt.from.toUpperCase(), accounts[0].toUpperCase());
+
+    // get doctor info and make sure it's correct (check doctor name and empty patient list)
+    const d = await doctorInstance.getDoctorInfo();
+    console.log(d);
+    assert.equal(d[0], 'Dionysios Kotsonis');
+    console.log(typeof(d[1]));
+    assert.equal(d[1], 0);
+  });
+
+  // Patient tests
+  it('creates a new patient by calling the signupPatient method', async () => {
+    // Wait for the contract to be deployed
+    const patientInstance = await Service.deployed();
+    // Call signupGreeting function
+    const patient1 = await patientInstance.signupPatient('Dionysios Kotsonis', 12);
+    // 
+    console.log("======================");
+    console.log(typeof(accounts[0]));
+    assert.equal(patient1.receipt.from.toUpperCase(), accounts[0].toUpperCase());
+
+    const p = await patientInstance.getPatientInfo();
+    console.log(p);
+    assert.equal(p[0], 'Dionysios Kotsonis');
+    console.log(typeof(p[1]));
+    assert.equal(p[2], 0);
+}); 
+
+  // File tests
+  it('uses testFile method to check deployment of File.sol', async () => {
+    
+    // Wait for the contract to be deployed
+    const fileInstance = await Service.deployed();
+    // Call testFile function
+    const tfile = await fileInstance.testFile("HIV: Negative|Hepatitus C: Negative|Coronavirus: Positive","blood test 1");
+    // 
+    console.log("======================");
+    console.log(tfile)
+
+    // get file hash, and file name from that
+    const hash = await fileInstance.getFileHash("HIV: Negative|Hepatitus C: Negative|Coronavirus: Positive");
+    const file_name = await fileInstance.getFileName(hash);
+    
+    // make sure the file name returns properly
+    assert.equal(file_name,"blood test 1");
   });
 });
