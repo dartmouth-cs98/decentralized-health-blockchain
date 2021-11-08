8bc19b6d367f638b12bcb7a5a30f1d79c8a56e06
Edie Wilson <ewilson@flyvoly.com>
2021-11-08 14:41:20 -0500

    wrote tests for adding files, verified proper file storage

--- test/helloworld.js
+++ test/helloworld.js
@@ -1,13 +1,13 @@
-const HelloWorld = artifacts.require("HelloWorld");
+// const HelloWorld = artifacts.require("HelloWorld");
 
-contract('HelloWorld', (accounts) => {
-  // Test HelloWorld.sol contract
-  it('greeting variable should be initialized to "Hello World!"', async () => {
-    // Wait for the contract to be deployed
-    const helloWorldInstance = await HelloWorld.deployed();
-    // Call getGreeting function
-    const greeting = await helloWorldInstance.getGreeting();
-    // Check the value is initialized.
-    assert.equal(greeting, "Hello World!", "String incorrectly initialized.");
-  });
-});
+// contract('HelloWorld', (accounts) => {
+//   // Test HelloWorld.sol contract
+//   it('greeting variable should be initialized to "Hello World!"', async () => {
+//     // Wait for the contract to be deployed
+//     const helloWorldInstance = await HelloWorld.deployed();
+//     // Call getGreeting function
+//     const greeting = await helloWorldInstance.getGreeting();
+//     // Check the value is initialized.
+//     assert.equal(greeting, "Hello World!", "String incorrectly initialized.");
+//   });
+// });
