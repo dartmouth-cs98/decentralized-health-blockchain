c5eb972bd080c0532c780e59f5390b4ea1fbb4b9
kevinlcaiug <kevin.l.cai.ug@dartmouth.edu>
2021-11-08 16:25:21 -0500

    Removed ConvertLib.sol contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
 const Service = artifacts.require("Service");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
   deployer.deploy(Service);
 };
