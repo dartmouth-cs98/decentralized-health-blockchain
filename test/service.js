32419031864bf5cfe522beb8b7845011cd60a767
kevinlcaiug <kevin.l.cai.ug@dartmouth.edu>
2021-11-08 01:28:59 -0500

    Changed addFile method to hash contents of file in the function, started testing for addFile method

--- test/service.js
+++ test/service.js
@@ -13,4 +13,18 @@ contract('Service', (accounts) => {
     console.log(tservice);
     assert.equal(tservice.toUpperCase(), accounts[0].toUpperCase());
   });
+
+  it('tests addFile method creates a new file in patients files array', async () => {
+    const serviceInstance = await Service.deployed();
+    console.log("1");
+    // create patient instance
+    const p = await serviceInstance.signupPatient("Dionysios Kotsonis", 12);
+    console.log("2");
+    // create doctor instance
+    const d = await serviceInstance.signupDoctor("Kevin Cai", 21);
+    console.log("3");
+    // add file to patient's file list
+    const f = await serviceInstance.addFile("Blood Test 1", "Blood Test", );
+    console.log("4");
+  });
 });
